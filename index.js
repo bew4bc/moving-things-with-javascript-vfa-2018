f92af0c16324d786a77292ad4b53683597c2ba9b
Ben Weinberg <bweinberg1222@gmail.com>
2018-06-24 20:06:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,8 +25,5 @@ function moveDodgerRight() {
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     moveDodgerLeft()
-  if (e.which === 39) {
-    moveDodgerRight()
-  }
   }
 })
